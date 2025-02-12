7be8914fa76717ccb6629e6c0da0aa7e2bd808c9
Lucas-wyz <49265926+Lucas-wyz@users.noreply.github.com>
2025-02-12 17:57:18 +0800

    初始化

---
+++ Admin.Free/AppDbContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Admin.Free
+{
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+    }
+}
