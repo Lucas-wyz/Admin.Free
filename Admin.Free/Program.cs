6a8aa8f07673aa5f576110a5bf153c920328550c
Lucas-wyz <49265926+Lucas-wyz@users.noreply.github.com>
2025-02-14 00:01:23 +0800

    add EFCore

--- Admin.Free/Program.cs
+++ Admin.Free/Program.cs
@@ -1,3 +1,6 @@
+using Admin.Free;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//mongodb for ef
+builder.Services.AddDbContext<AppDbContext>((service, options) =>
+{
+	options.UseMongoDB(service.GetService<IConfiguration>().GetConnectionString("mongodb"), "appdb");
+});
 
 var app = builder.Build();
 
