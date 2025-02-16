af20f0cc29cc84baf94fd6a03708b848199fdd53
Lucas-wyz <49265926+Lucas-wyz@users.noreply.github.com>
2025-02-16 17:08:21 +0800

    Add  GlobalExceptionFilter

--- Admin.Free/Program.cs
+++ Admin.Free/Program.cs
@@ -1,4 +1,5 @@
 using Admin.Free;
+using Admin.Free.Filter;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 
@@ -6,7 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<GlobalExceptionFilterAttribute>();
+}).AddNewtonsoftJson();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
