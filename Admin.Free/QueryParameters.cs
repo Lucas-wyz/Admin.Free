e8ffe6b910ee671c28dfbf37cd394cfd2fcb8966
Lucas-wyz <49265926+Lucas-wyz@users.noreply.github.com>
2025-02-25 23:53:04 +0800

    Add   添加查询参数

---
+++ Admin.Free/QueryParameters.cs
@@ -0,0 +1,15 @@
+namespace Admin.Free
+{
+	/// <summary>
+	/// ²éÑ¯²ÎÊı
+	/// </summary>
+	public class QueryParameters
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		public string? ID { get; set; }
+
+	}
+
+}
