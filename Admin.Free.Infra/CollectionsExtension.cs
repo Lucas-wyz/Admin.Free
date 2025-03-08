c3106d32d60a77888ee0a830bb4ea2fc97625a7d
Lucas-wyz <49265926+Lucas-wyz@users.noreply.github.com>
2025-03-08 23:58:51 +0800

    Add  Extension

---
+++ Admin.Free.Infra/CollectionsExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace Admin.Free.Infra
+{
+	public static class CollectionsExtension
+	{
+		public static IEnumerable<T> WhereIf<T>(IEnumerable<T> list, bool tag, Func<T, bool> func)
+		{
+			if (tag)
+			{
+				return list.Where(func);
+			}
+			return list;
+
+		}
+	}
+}
