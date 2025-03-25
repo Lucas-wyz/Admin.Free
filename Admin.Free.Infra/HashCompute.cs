6e8527b10b071d89c4fe05bdf31ed1d7d35d9d39
Lucas-wyz <49265926+Lucas-wyz@users.noreply.github.com>
2025-03-25 23:57:43 +0800

    Add  ComputeHash

---
+++ Admin.Free.Infra/HashCompute.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Admin.Free.Infra
+{
+	public class HashCompute
+	{
+
+		public string GetMd5(string str)
+		{ 
+			var byrearr = Encoding.UTF8.GetBytes(str);
+
+			var md5 = MD5.Create();
+
+			var hash = md5.ComputeHash(byrearr);
+			 
+			var hashstr = string.Join("", hash.Select(x => x.ToString("x2")));
+
+			return hashstr;
+		}
+
+
+		public string GetSHA1(string str)
+		{
+
+			var byrearr = Encoding.UTF8.GetBytes(str);
+
+			var sha1 = SHA1.Create();
+
+			var hash = sha1.ComputeHash(byrearr);
+
+			var hashstr = string.Join("", hash.Select(x => x.ToString("x2")));
+
+			return hashstr;
+		}
+
+	}
+}
