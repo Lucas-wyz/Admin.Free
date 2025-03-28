eb0de8ff8ceb406972b0fa5c35d5ec4cbe668288
Lucas-wyz <49265926+Lucas-wyz@users.noreply.github.com>
2025-03-28 23:57:55 +0800

    Add  GetRandom

--- Admin.Free/Controllers/QuestionsController.cs
+++ Admin.Free/Controllers/QuestionsController.cs
@@ -99,6 +99,20 @@ namespace Admin.Free.Controllers
 
 	
 
+		[HttpGet("GetRandom")]
+		public ResultObjet<QuestionsView> GetRandom([FromQuery] QueryParameters queryParameters)
+		{
+			var configMap = new MapperConfiguration(cfg =>
+			cfg.CreateMap<Questions, QuestionsView>().ForMember(x => x.options, o => o.MapFrom(s => dbc.QuestionOptions.Where(y => y.QuestionID == s.ID).ToList())));
+
+			var list = dbc.Questions.Page(queryParameters.Page, queryParameters.Size).First();
+
+			var listView = configMap.CreateMapper().Map<Questions, QuestionsView>(list);
+
+			return this.OKResult(listView);
+
+		}
+
 		/// <summary>
 		/// 
 		/// </summary>
