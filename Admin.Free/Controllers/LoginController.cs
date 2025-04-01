9b4872341eddf11cac5590b915202f5f0b575066
Lucas-wyz <49265926+Lucas-wyz@users.noreply.github.com>
2025-04-01 23:57:03 +0800

    Add   Login

---
+++ Admin.Free/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using Admin.Free.Extensions;
+using Admin.Free.Infra;
+using Admin.Free.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Admin.Free.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class LoginController(AppDbContext dbc, ILogger<LoginController> logger) : ControllerBase
+	{
+
+		[HttpPost]
+		public ResultObjet<string> Post([FromBody] Users obj)
+		{
+
+			var user = dbc.Users.Where(x => x.Name == obj.Name && x.Phone == obj.Phone).First();
+
+			return this.OKResult("token");
+		}
+
+	}
+
+
+
+}
