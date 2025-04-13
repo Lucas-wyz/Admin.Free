37d1c1601fe16d8231b25fb7f551200d20f014b4
Lucas-wyz <49265926+Lucas-wyz@users.noreply.github.com>
2025-04-14 02:51:40 +0800

    Fix Jwt

---
+++ Admin.Free/JwtConfig.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Admin.Free
+{
+	/// <summary>
+	/// jwt
+	/// </summary>
+	public class JwtConfig
+	{
+		/// <summary>
+		/// 发行人Issuer
+		/// </summary>
+		public static readonly string issuer = "free";
+		/// <summary>
+		/// 订阅人Audience
+		/// </summary>
+		public static readonly string audience = "free";
+		/// <summary>
+		/// SecurityKey
+		/// </summary>
+		public static readonly string secretKey = "egh5t2eh545j2jsj575eisrtgxtrkmhj40wmsh54h21";
+
+		/// <summary>
+		/// SecurityKeyBytes
+		/// </summary>
+		public static readonly byte[] secretKeyBytes = Encoding.UTF8.GetBytes(secretKey);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="claims"></param>
+		/// <returns></returns>
+		public static string CreateToken(IEnumerable<Claim> claims = null)
+		{
+
+			// 1. 定义需要使用到的Claims
+			claims = claims ?? new List<Claim>();
+			claims.Append(new Claim(JwtRegisteredClaimNames.Jti, "admin"));
+
+			// 2. 读取SecretKey
+			var secretKey = new SymmetricSecurityKey(JwtConfig.secretKeyBytes);
+
+			// 3. 选择加密算法
+			var algorithm = SecurityAlgorithms.HmacSha256;
+
+			// 4. 生成Credentials
+			var signingCredentials = new SigningCredentials(secretKey, algorithm);
+
+			// 5. 根据以上，生成token
+			var jwtSecurityToken = new JwtSecurityToken(issuer: issuer, audience: audience, claims: claims, notBefore: DateTime.Now, expires: DateTime.Now.AddHours(30), signingCredentials: signingCredentials);
+
+			// 6. 将token变为string
+			var token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+
+			return token;
+		}
+	}
+
+
+}
--- Admin.Free/Program.cs
+++ Admin.Free/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Serilog;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Admin.Free.Models;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,7 +20,30 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddHttpContextAccessor();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
+
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+	.AddJwtBearer(jwtOptions =>
+	{
+
+		jwtOptions.TokenValidationParameters = new TokenValidationParameters()
+		{
+			ValidateIssuer = false, //是否验证Issuer
+			ValidIssuer = JwtConfig.issuer,//configuration["Jwt:Issuer"], //发行人Issuer
+			ValidateAudience = true, //是否验证Audience
+			ValidAudience = JwtConfig.audience,// configuration["Jwt:Audience"], //订阅人Audience
+			ValidateIssuerSigningKey = true, //是否验证SecurityKey
+			IssuerSigningKey = new SymmetricSecurityKey(JwtConfig.secretKeyBytes), //SecurityKey
+
+			ValidateLifetime = false, //是否验证失效时间
+			ClockSkew = TimeSpan.FromSeconds(30), //过期时间容错值，解决服务器端时间不同步问题（秒）
+			RequireExpirationTime = true,//必须具有“过期”值
+		};
+
+	});
+
+
 //Serilog
 builder.Services.AddSerilog((service, logger) =>
 {
