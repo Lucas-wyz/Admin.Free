09b41d7ac83a640186f9611f1394d6c57bbece07
Lucas-wyz <49265926+Lucas-wyz@users.noreply.github.com>
2025-05-20 23:59:28 +0800

    Fix  CollectionsExtension

--- Admin.Free.Infra/CollectionsExtension.cs
+++ Admin.Free.Infra/CollectionsExtension.cs
@@ -5,7 +5,7 @@ namespace Admin.Free.Infra
 {
 	public static class CollectionsExtension
 	{
-		public static IEnumerable<T> WhereIf<T>(IEnumerable<T> list, bool tag, Func<T, bool> func)
+		public static IEnumerable<T> WhereIf<T>(this IEnumerable<T> list, bool tag, Func<T, bool> func)
 		{
 			if (tag)
 			{
@@ -14,6 +14,18 @@ namespace Admin.Free.Infra
 			return list;
 
 		}
+
+        public static IQueryable<T> WhereIf<T>(this IQueryable<T> list, bool tag, Expression<Func<T, bool>> func)
+        {
+            if (tag)
+            {
+                return list.Where(func);
+            }
+            return list;
+
+        }
+
+
 		public static IEnumerable<TDestination> ProjectTo<TDestination>(this IEnumerable source, IConfigurationProvider configuration)
 		{
 			return configuration.CreateMapper().Map<IEnumerable<TDestination>>(source);
