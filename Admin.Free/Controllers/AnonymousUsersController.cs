a3af192e6a10acd897cfd789265ea4b4230ada2b
Lucas-wyz <49265926+Lucas-wyz@users.noreply.github.com>
2025-07-31 00:00:00 +0800

    Fix

--- Admin.Free/Controllers/AnonymousUsersController.cs
+++ Admin.Free/Controllers/AnonymousUsersController.cs
@@ -13,13 +13,6 @@ namespace Admin.Free.Controllers
     {
         public MapperConfiguration _configMap => new MapperConfiguration(cfg =>
             {
-                cfg.CreateMap<Questions, QuestionsView>()
-                .ForMember(x => x.options, o => o.MapFrom(s => dbc.QuestionOptions.Where(y => y.QuestionID == s.ID).ToList()))
-                .ForMember(x => x.correct_answer, o => o.MapFrom(s => dbc.QuestionOptions.Where(y => y.QuestionID == s.ID).Where(y => y.correct == true).Select(x => x.option_text).ToList()));
-
-                cfg.CreateMap<Questions, QuestionsView>().ForMember(x => x.tags, o => o.MapFrom(s => s.tags == null ? null : s.tags.Split(new char[] { ',' })));
-                cfg.CreateMap<QuestionsView, Questions>().ForMember(x => x.tags, o => o.MapFrom(s => s.tags == null ? null : string.Join(',', s.tags)));
-
             });
 
 
