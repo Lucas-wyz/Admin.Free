eb686e184f48a00784e35f7224015d2797619a87
Lucas-wyz <49265926+Lucas-wyz@users.noreply.github.com>
2025-08-07 21:59:09 +0800

    Fix    expression  extension

--- Admin.Free.Infra/ExpressionableEX.cs
+++ Admin.Free.Infra/ExpressionableEX.cs
@@ -85,4 +85,75 @@ namespace Admin.Free.Infra
         }
     }
 
+
+    public class ExpressionableEX<T>
+    {
+        private readonly Type _type;
+        private readonly ParameterExpression _parameter;
+        private List<(Func<Expression, Expression, BinaryExpression> func, Expression expr)> expressions = new();
+        private ExpressionableEX()
+        {
+            var type = typeof(T);
+            _type = type;
+            _parameter = Expression.Parameter(type);
+        }
+
+        public static ExpressionableEX<T> Create()
+        {
+            return new ExpressionableEX<T>();
+        }
+
+        /// <summary>
+        /// AND
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="propname"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public ExpressionableEX<T> AndAlso(Expression<Func<T, bool>> predicate)
+        {
+            var br = Expression.Invoke(predicate, _parameter);
+            var andFunc = (Expression left, Expression right) => Expression.AndAlso(left, right);
+            expressions.Add((andFunc, br));
+            return this;
+        }
+
+        /// <summary>
+        /// OR
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="propname"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public ExpressionableEX<T> OrElse(Expression<Func<T, bool>> predicate)
+        {
+            var br = Expression.Invoke(predicate, _parameter);
+            var andFunc = (Expression left, Expression right) => Expression.OrElse(left, right);
+            expressions.Add((andFunc, br));
+            return this;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public Expression<Func<T, bool>> ToLambda()
+        {
+            //default
+            Expression<Func<T, bool>> lambda = (T t) => true;
+            if (expressions.Count() > 0)
+            {
+                var _body = expressions[0]!.expr;
+                foreach (var item in expressions[1..])
+                {
+                    _body = item.func(_body, item.expr);
+                }
+                lambda = Expression.Lambda<Func<T, bool>>(_body, _parameter);
+            }
+
+            return lambda;
+        }
+
+    }
+
 }
